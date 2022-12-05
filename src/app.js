9a194a3920936d3fad7fe8dbc8ea9c97a9ff2095
carlosguilhermeklein <carlosguilhermeklein@hotmail.com>
2022-12-05 19:07:06 -0300

    paginas login em ejs

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var produtoRouter = require('./routes/produto');
 var produtoDetalhesRouter = require('./routes/produto_detalhes');
 var cadastroUsuarioRouter = require('./routes/cadastro_usuario');
+var loginRouter = require('./routes/login');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/', produtoRouter);
 app.use('/', produtoDetalhesRouter);
 app.use('/', cadastroUsuarioRouter);
+app.use('/', loginRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
