9b9a8c30474c34fd7322de5e92b3ecd47bab7821
carlosguilhermeklein <carlosguilhermeklein@hotmail.com>
2022-12-09 19:58:26 -0300

    carrinho finalizado

--- src/routes/carrinho.js
+++ src/routes/carrinho.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var page ='carrinho' 
 /* GET home page. */
 router.get(('/' + page), function(req, res, next) {
-  res.render( page, { title: 'DIY - Gamer Solutions',css:'stylesheets/'+page+'.css' });
+  res.render( page, { title: 'DIY - Gamer Solutions',css:'stylesheets/'+page+'.css',js:'javascripts/'+page+'.js' });
 });
 
 module.exports = router;
