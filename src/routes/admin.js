fa9e835bf95c63420f9eb87558301d7348cbce72
Tânia Maria de Oliveira <oliveiratania11@gmail.com>
2023-01-31 15:44:07 -0300

    criando página admin para CRUD

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var page ='admin' 
+var page ='admin'
+
+const maquinas = [
+  { id: 1, titulo: "Hover 22", placa: "Nvidea12"},
+  { id: 2, titulo: "UHT DX", placa: "Nvidea13"},
+  { id: 4, titulo: "DNH 147", placa: "Nvidea14"},
+  { id: 5, titulo: "USER 147", placa: "Nvidea15"}
+]
+
 /* GET home page. */
 router.get(('/' + page), function(req, res, next) {
-  res.render( page, { title: 'DIY - Gamer Solutions',css:'stylesheets/'+page+'.css' });
+  res.render( page, { title: 'DIY - Gamer Solutions',css:'stylesheets/'+page+'.css', maquinas });
 });
 
 module.exports = router;
