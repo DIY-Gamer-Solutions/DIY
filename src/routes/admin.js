2f51302d8c8016d291dd138596ecf25690b108dd
Tânia Maria de Oliveira <oliveiratania11@gmail.com>
2023-02-02 22:34:40 -0300

    CRUD - deletar e criar produto feitos. Faltam ajustes.

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,21 +1,47 @@
 var express = require('express');
 var router = express.Router();
 var page ='admin'
+const fs = require('fs');
+const path = require('path');
 
-const maquinas = [
-  { id: 1, titulo: "Hover 22", placa: "Nvidea12"},
-  { id: 2, titulo: "UHT DX", placa: "Nvidea13"},
-  { id: 4, titulo: "DNH 147", placa: "Nvidea14"},
-  { id: 5, titulo: "USER 147", placa: "Nvidea15"}
-]
+
+const maquinasPath = path.resolve(__dirname, '../database/produtos.json')
+
+
+function getMaquinas () {
+  return JSON.parse(fs.readFileSync(maquinasPath));
+}
+
+function saveMaquinas (maquinas) {
+  fs.writeFileSync(maquinasPath, JSON.stringify(maquinas,null, 4));
+}
+
 
 /* GET home page. */
 router.get(('/' + page), function(req, res, next) {
-  res.render( page, { title: 'DIY - Gamer Solutions',css:'stylesheets/'+page+'.css', maquinas });
+  res.render( page, { title: 'DIY - Gamer Solutions',css:'stylesheets/'+page+'.css', maquinas:getMaquinas()});
 });
 
 router.get('/adicionar', (req, res) => {
-  res.render('adicionarMaquina', {css: 'stylesheets/admin.css', title:'adicionarMaquina'});
+  res.render('adicionarMaquina', {css: 'stylesheets/adicionarMaquina.css', title:'DIY - Gamer Solutions'});
+});
+
+router.post('/' + page, function(req, res){
+  const maquinas = getMaquinas();
+  maquinas.push({ id: maquinas.at(-1).id + 1, ...req.body });
+
+  saveMaquinas(maquinas);
+  res.redirect('/admin');
+});
+ 
+router.delete('/deletar/:id', (req, res) => {
+  const id = req.params.id;
+  let maquinas = getMaquinas();
+
+  maquinas = maquinas.filter(maquina => maquina.id != id);
+
+  saveMaquinas(maquinas);
+  res.redirect('/admin');
 });
 
 module.exports = router;
